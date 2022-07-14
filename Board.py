214f60121847146788aa334426996729072b8636
Gottlieb613 <charliegottlieb@gmail.com>
2022-07-14 15:47:23 +0300

    Added next_tile to Board and text repr

--- Board.py
+++ Board.py
@@ -3,6 +3,9 @@ class Board:
         class TileNode:
             def __init__(self):
                 self.player = 0
+            
+            def __repr__(self):
+                return str(self.player)
 
         def __init__(self):
             self.player = 0
@@ -13,7 +16,7 @@ class Board:
         self.win = False
     
     def update(self, box, tile, new_val):
-        self.boxes[box][tile].player = new_val
+        self.boxes[box].tiles[tile].player = new_val
     
     def update_box(self, box, new_val):
         self.boxes[box].player = new_val
@@ -56,5 +59,64 @@ class Board:
 
         #no wins found so return 0
         return 0
+
+    def __repr__(self):
+        rep = ""
+        next = 0, 0
+        while next is not None:
+            next_box = next[0]
+            next_tile = next[1]
+            
+            rep += str(self.boxes[next_box].tiles[next_tile])
+            if (next_tile % 3 == 2):
+                rep += " "
+            if next_box % 3 == 2 and next_tile % 3 == 2:
+                rep += "\n"
+                if next_tile == 8:
+                    rep += "\n"
+
+            next = self.next_tile(next[0], next[1])
+
+        return rep
+
+    #The idea for this function is that it will return
+    # the box, tile pair for the tile to the RIGHT
+    # of the input one, and if it is at the end of the 
+    # 9-tile row, then it goes to the next row
+    #Returns None if given the bottom right tile of the board
+    def next_tile(self, box, tile):
+        box_row = box / 3
+        box_col = box % 3
+        tile_row = tile / 3
+        tile_col = tile % 3
+
+        if (tile_col < 2): #stay in box-> just move right
+            return box, tile + 1
+
+        else: #so we're in the rightmost col in the box
+            if box_col < 2: #stay in box-row -> move to corresp. row in box to right
+                return box + 1, tile - 2 #tile-2 allows us to go to 'start' of row in box
+
+            else: #rightmost box of the row
+                if tile_row < 2: #so just move to leftmost box of same row and next tile-row
+                    return box - 2, tile + 1
+                
+                else: #bottom right of the rightmost box
+                    if box_row < 2: #so we're NOT in the bottom right box
+                        return box + 1, 0   #zero because we start at top left of new box
+                    
+                    else: #literally ONLY the bottom right tile of bottom right box, so end here
+                        return None
+
+
+
+b = Board()
+b.update(4, 2, 1)
+b.update(5, 1, 1)
+b.update(8, 4, 2)
+
+print(b)
+
+
                 
     
